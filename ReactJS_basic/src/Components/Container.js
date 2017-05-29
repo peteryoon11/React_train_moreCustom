9384e3cf84a9c8bbcdb6e4caa9a3d04a74744f23
jisub yoon <yjisub11@gmail.com>
2017-05-29 16:56:07 +0900

    tranning start with webpack babel hot-loader webpack-dev-server

---
+++ ReactJS_basic/src/Components/Container.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import App from './App.js';
+
+
+const Container = () => {
+
+    const itemList=items.map(
+        ()=>(
+            <TodoItem/>
+        )
+    )
+
+
+    return (
+        <div>
+         
+        </div>
+    );
+};
+
+export default Container;
